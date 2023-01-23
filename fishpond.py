d24be6d856e4f9b6f6b8a932f5e3a1c27ab8d398
Jason King <57264802+MrJasonKing@users.noreply.github.com>
2023-01-23 11:06:20 +0700

    Update fishpond.py

--- fishpond.py
+++ fishpond.py
@@ -1,7 +1,7 @@
 print("Fish Pond Model")
 print("==============")
-widht = float(input("Enter pond width (meters): "))
-length = float(input("Enter pond lenght (meters): "))
+width = float(input("Enter pond width (meters): "))
+length = float(input("Enter pond length (meters): "))
 depth = float(input("Enter pond depth (meters): "))
 print("\n")
 print("Results")
@@ -9,7 +9,7 @@ print("-------")
 area = width * length
 print("Surface area of the pond is", area, "square meters")
 volume = area * depth
-print("Pond contains", volume, "cubic meteres of water")
+print("Pond contains", volume, "cubic meters of water")
 fish = volume * 2
 fins = int(fish)
 print("Number of fish: ", fish)
